8a663ba77fdf63dc9f3aa1feebb70e173c0f31cb
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-03-04 22:06:06 -0500

    Primner Commi

---
+++ src/main/java/com/repaso/test/Test.java
@@ -0,0 +1,14 @@
+package com.repaso.test;
+
+import com.repaso.entidades.Persona;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		Persona p = new Persona("Jean", "Marchesini", 24);
+		System.out.println(p);
+		
+	}
+
+}
