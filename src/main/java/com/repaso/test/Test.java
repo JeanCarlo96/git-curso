fb3b7b8e40ff7b84cb1ab9b4f8a5b99224032d33
JeanCarlo96 <jeancarlo.marchesini@yahoo.es>
2021-03-04 22:13:01 -0500

    Modificación en el test añadiendo una nueva Persona

--- src/main/java/com/repaso/test/Test.java
+++ src/main/java/com/repaso/test/Test.java
@@ -9,6 +9,9 @@ public class Test {
 		Persona p = new Persona("Jean", "Marchesini", 24);
 		System.out.println(p);
 		
+		Persona p2 = new Persona("Gissela", "Sangoquiza", 26);
+		System.out.println(p2);
+		
 	}
 
 }
